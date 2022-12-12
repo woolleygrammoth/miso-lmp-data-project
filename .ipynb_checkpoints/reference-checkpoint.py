d57480e7f4cf99c98377644442abae47d568f50e
Graham Woolley <gwoolley@berkeley.edu>
2022-12-12 09:57:08 -0800

    initial commit

---
+++ .ipynb_checkpoints/reference-checkpoint.py
@@ -0,0 +1,2 @@
+link = 'https://www.misoenergy.org/markets-and-operations/real-time--market-data/market-reports/#nt=%2FMarketReportType%3AHistorical%20LMP%2FMarketReportName%3AWeekly%20Real-Time%205-Min%20LMP%20(zip)&t=500&p=0&s=MarketReportPublished&sd=desc'
+
